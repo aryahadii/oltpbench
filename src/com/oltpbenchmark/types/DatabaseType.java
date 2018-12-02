30d990a3f1187b7d9ba07718570dc0406eaca9fc
Arya Hadi <arya.hadi97@gmail.com>
2018-12-02 20:38:35 +0330

    feat: add VoltDB to supported databases

--- src/com/oltpbenchmark/types/DatabaseType.java
+++ src/com/oltpbenchmark/types/DatabaseType.java
@@ -21,7 +21,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * List of the database management systems that we support 
+ * List of the database management systems that we support
  * in the framework.
  * @author pavlo
  */
@@ -52,8 +52,9 @@ public enum DatabaseType {
     CASSANDRA("com.github.adejanovski.cassandra.jdbc.CassandraDriver", true, true, false),
     PELOTON("org.postgresql.Driver", false, false, true),
     MEMSQL("com.mysql.jdbc.Driver", true, false, false),
+    VOLTDB("org.voltdb.jdbc.Driver", true, false, false),
     ;
-    
+
     private DatabaseType(String driver,
                          boolean escapeNames,
                          boolean includeColNames,
@@ -63,25 +64,25 @@ public enum DatabaseType {
         this.includeColNames = includeColNames;
         this.supportTxns = supportTxns;
     }
-    
+
     /**
      * This is the suggested driver string to use in the configuration xml
-     * This corresponds to the <B>'driver'</b> attribute. 
+     * This corresponds to the <B>'driver'</b> attribute.
      */
     private final String driver;
-    
+
     /**
      * If this flag is set to true, then the framework will escape names in
-     * the INSERT queries  
+     * the INSERT queries
      */
     private final boolean escapeNames;
-    
+
     /**
      * If this flag is set to true, then the framework will include the column names
-     * when generating INSERT queries for loading data.  
+     * when generating INSERT queries for loading data.
      */
     private final boolean includeColNames;
-    
+
     /**
      * If this flag is set to true, then the framework will invoke the JDBC transaction
      * api to do various things during execution. This should only be disabled
@@ -90,11 +91,11 @@ public enum DatabaseType {
      * when the framework tries to set the isolation level.
      */
     private boolean supportTxns;
-    
+
     // ---------------------------------------------------------------
     // ACCESSORS
     // ----------------------------------------------------------------
-    
+
     /**
      * Returns the suggested driver string to use for the given database type
      * @return
@@ -102,25 +103,25 @@ public enum DatabaseType {
     public String getSuggestedDriver() {
         return (this.driver);
     }
-    
+
     /**
-     * Returns true if the framework should escape the names of columns/tables when 
+     * Returns true if the framework should escape the names of columns/tables when
      * generating SQL to load in data for the target database type.
      * @return
      */
     public boolean shouldEscapeNames() {
         return (this.escapeNames);
     }
-    
+
     /**
-     * Returns true if the framework should include the names of columns when 
+     * Returns true if the framework should include the names of columns when
      * generating SQL to load in data for the target database type.
      * @return
      */
     public boolean shouldIncludeColumnNames() {
         return (this.includeColNames);
     }
-    
+
     /**
      * Returns true if the framework should use transactions when executing
      * any SQL queries on the target DBMS.
@@ -129,11 +130,11 @@ public enum DatabaseType {
     public boolean shouldUseTransactions() {
         return (this.supportTxns);
     }
-    
+
     // ----------------------------------------------------------------
     // STATIC METHODS + MEMBERS
     // ----------------------------------------------------------------
-    
+
     protected static final Map<Integer, DatabaseType> idx_lookup = new HashMap<Integer, DatabaseType>();
     protected static final Map<String, DatabaseType> name_lookup = new HashMap<String, DatabaseType>();
     static {
@@ -142,7 +143,7 @@ public enum DatabaseType {
             DatabaseType.name_lookup.put(vt.name().toUpperCase(), vt);
         }
     }
-    
+
     public static DatabaseType get(String name) {
         DatabaseType ret = DatabaseType.name_lookup.get(name.toUpperCase());
         return (ret);
